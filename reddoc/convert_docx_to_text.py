a40e135f96421cee7a1e44fddd73b5f70d97e1cd
tmwsiy <tmwsiy2012@gmail.com>
2014-08-31 02:10:08 -0400

    reading docx to text

--- reddoc/convert_docx_to_text.py
+++ reddoc/convert_docx_to_text.py
@@ -1,8 +1,12 @@
 __author__ = 'tmwsiy'
 
 from docx import Document
+import io
+import sys
+import re
+import copy
 
-previous_bold_state=False
+previous_bold_state = False
 
 def check_bodmer_begin(state_to_check):
     global previous_bold_state
@@ -11,14 +15,88 @@ def check_bodmer_begin(state_to_check):
     else:
         return False
 
+def read_chap_verse(chap, verse, dir_prefix='', output_to=sys.stdout):
+    global previous_bold_state
+    document = Document(dir_prefix+'Stg7 ' + '%02d' % (chap)+ ' ' + '%02d' %  (verse) +'.docx')
+    doc_string = io.StringIO()
+    if output_to is None:
+        output_to = doc_string
+    for paragraph in document.paragraphs:
+        print('',file=output_to)
+        for run in paragraph.runs:
+            if check_bodmer_begin(run.bold) == True:
+                print('Bodmer: '+run.text, end='', file=output_to)
+            else:
+                print(run.text, end='', file=output_to)
+            previous_bold_state=run.bold
+    return doc_string.getvalue()
 
-document = Document('Stg7 01 01.docx')
+def get_chap_verse(chap, verse, dir_prefix=''):
+    return read_chap_verse(chap, verse, dir_prefix='', output_to=None)
 
-for paragraph in document.paragraphs:
-    print('')
-    for run in paragraph.runs:
-        if check_bodmer_begin(run.bold) == True:
-            print('Bodmer: '+run.text, end='')
-        else:
-            print(run.text, end='')
-        previous_bold_state=run.bold
+def print_chap_verse(chap, verse, dir_prefix=''):
+    read_chap_verse(chap, verse, dir_prefix='', output_to=sys.stdout)
+
+def split_on_number_boundary(string_to_split):
+    m = re.search("\d", string_to_split)
+    if m:
+        index = m.start()
+        return [ string_to_split[:index].strip(), string_to_split[index:].strip()]
+    else:
+        return []
+
+def listify_manuscript_list(input_list):
+    text_reading = input_list[0]
+    manuscript_list = input_list[1].split()
+    for manuscript in range(len(manuscript_list)):
+        # need to fix 2 digit manuscripts to add leading zero
+        if len(manuscript_list[manuscript]) == 2:
+            manuscript_list[manuscript] = '0'+ manuscript_list[manuscript]
+    return [ text_reading, manuscript_list]
+
+def listify_apparatus_verse_document(raw_utf8):
+    current_bodmer_line = ''
+    # ffb is "found first bodmer" passage in verse document
+    ffb=False
+    listified_document = []
+    previous_line=''
+    for line in str.splitlines(raw_utf8):
+        if line.startswith('Bodmer:'):
+            ffb=True
+            current_bodmer_line = ['B',line[8:].strip()]
+            listified_document.append(current_bodmer_line)
+        else: # Not a bodmer line
+            split_line = split_on_number_boundary(line)
+            if len(split_line) > 0 and len(split_line[0]) == 0 and ffb==True:
+                # bodmer manuscript list
+                split_line[0] = 'BML'
+                listified_document.append(listify_manuscript_list(split_line))
+            elif len(split_line) == 0:
+                pass
+            else:
+                # "normal" split line
+                # check for mising BML if bodmer was only reading and insert blank one
+                if previous_line.startswith('Bodmer:'):
+                   listified_document.append(['BML',[]])
+                listified_document.append(listify_manuscript_list(split_line))
+        previous_line = line
+    return listified_document
+def post_process_listified_document(raw_list):
+    # make a copy to manipulate
+    temp_list = copy.deepcopy(raw_list)
+    # must reset everytime we make a change
+    # while not finished:
+    for i in range(1,len(temp_list)):
+        print(temp_list[i])
+
+#print_chap_verse(1,1)
+for i in range(1,14):
+    post_process_listified_document(listify_apparatus_verse_document(get_chap_verse(1,i)))
+
+
+'''
+output = cStringIO.StringIO()sudop
+output.write('First line.\n')
+print >>output, 'Second line.'
+previous_bold_state=False
+'''
