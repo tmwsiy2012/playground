ee4d141ddce2d01e1aa2e608de9a2b0ec2049a70
tmwsiy <tmwsiy2012@gmail.com>
2014-09-22 12:01:26 -0400

    reading docx to text

--- net_poke/udp_emitter.py
+++ net_poke/udp_emitter.py
@@ -9,10 +9,17 @@ import socket
 UDP_IP = "127.0.0.1"
 
 UDP_PORT = 4239
+listen_port=4240
 
-#MESSAGE='WAKEUP:CI1003'
-MESSAGE='UPDATE'
+MESSAGE='WAKEUP:CI1003'
+#MESSAGE='UPDATE'
 
+reply_socket = socket.socket(socket.AF_INET, # Internet
+                      socket.SOCK_DGRAM) # UDP
+reply_socket.bind(('', listen_port))
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+
+data, addr = reply_socket.recvfrom(1024) # buffer size is 1024 bytes
+print "received:", data, "from", addr
--- net_poke/udp_receiver.py
+++ net_poke/udp_receiver.py
@@ -1,7 +1,7 @@
 import socket
 import urllib
 import zipfile
-import sys, struct, socket, tempfile, os, csv
+import sys, struct, socket, tempfile, os, csv, traceback
 from subprocess import call
 
 
@@ -10,11 +10,12 @@ from subprocess import call
 data_url = "http://www.csc.uncw.edu/mac_lists.zip"
 allowed_ips = ['127.0.0.1','152.20.244.74']
 listen_port=4239
-base_dir = '/home/tmwsiy/'
-data_dir = base_dir+'mac_lists/'
+response_port=4240
+
 broadcast = {'BR':['152.20.223.255'], 'CI':['152.20.234.255']}
-room_list = {}
+machines_to_wake = {}
 wol_port = 9
+
 
 def wakeup_room(mac_list, building_code):
     for ethernet_address in mac_list:
@@ -50,27 +51,32 @@ def wakeup_machine(ethernet_address, building_code):
          print 'Problem sending magic packet...', Exception, err
 
 
-def importMacs( room_number):
-    return [line.strip() for line in open(data_dir + room_number +".txt")]
-
 def update_data():
-    global room_list
+    global machines_to_wake
     machines_to_wake = {}
     tmp_zip = tempfile.mkdtemp()
     name, hdrs = urllib.urlretrieve(data_url, tmp_zip + '\mac_lists.zip')
     zip = zipfile.ZipFile(name)
     zip.extractall(path=tmp_zip)
     for f in os.listdir(os.path.join(tmp_zip,'mac_lists')):
-        print 'new file', f
         with open(os.path.join(tmp_zip,'mac_lists',f), "r") as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',', quotechar='"')
             for row in csv_reader:
                 if len(row) > 6 and not "Workstation" in row[0]:
-                    print row[0][:6], row[6]
+                    room = row[0][:6]
+                    mac_to_store = ':'.join(s.encode('hex') for s in row[6].decode('hex')).upper()
+                    if room in machines_to_wake:
+                        machines_to_wake[room].append(mac_to_store)
+                    else:
+                        machines_to_wake[room] = []
+                        machines_to_wake[room].append(mac_to_store)
 
-        #csv_file = open(os.path.join(tmp_zip,'mac_lists',f), "r")
+def send_response(MESSAGE, UDP_IP, UDP_PORT):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
 
-
+# pre populate data before start
+update_data()
 
 sock = socket.socket(socket.AF_INET, # Internet
                       socket.SOCK_DGRAM) # UDP
@@ -86,19 +92,25 @@ while True:
             command = str(data)
             if command.startswith("WAKEUP"):
                 print "Accepted Command:", command, "from", addr
-                room_number = command.split(":")[1][2:]
-                building_code = command.split(":")[1][:2]
-                num_attempted = wakeup_room( importMacs( room_number ), building_code)
-                print "Attempted:", str(num_attempted), "Room:", room_number
+                room = command.split(":")[1].strip()
+                #room_number = command.split(":")[1][2:]
+                building_code = room[:2]
+                num_attempted = wakeup_room( machines_to_wake[room], building_code)
+                print "Attempted:", str(num_attempted), "Room:", room
+                print addr
+                send_response("OK... Attempted: " + str(num_attempted) + " Room: " + room, addr[0], response_port)
             elif command.startswith("UPDATE"):
+                print "Accepted Command:", command, "from:", addr
                 update_data()
+                send_response("OK... data updated", addr[0], response_port)
             else:
-                print 'not understood command:', command
-
+                print 'invalid command:', command, "from:", addr
+                send_response("Command not understood", addr[0], response_port)
         else:
-            print "not allowed"
+            print "NOT ALLOWED payload:", str(data), "from:", addr
     except Exception, err:
-         print 'problem in main loop', Exception, err
+        traceback.print_exc()
+        print 'problem in main loop', Exception, err
 
 
 
