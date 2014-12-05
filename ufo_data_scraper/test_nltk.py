3f8cfcfa392efe6c7474793ee48df0241cca1e03
tmwsiy2012 <dk4Wk4nsXq>
2014-12-05 10:07:24 -0500

    adf

--- ufo_data_scraper/test_nltk.py
+++ ufo_data_scraper/test_nltk.py
@@ -2,6 +2,9 @@ __author__ = 'tmwsiy'
 
 import pymongo
 import nltk
+import tfidf
+from nltk.corpus import stopwords
+import cPickle as pickle
 from nltk import FreqDist
 from nltk.util import ngrams
 # Connection to Mongo DB
@@ -13,12 +16,55 @@ except pymongo.errors.ConnectionFailure as e:
    print("Could not connect to MongoDB: " + e)
 
 db = conn.corpus
-collection = db.sightings
+collection = db.sightings_geo
 
 
 tokens= []
+documents=[]
+global_doc={}
+results={}
+
 for post in collection.find():
-    tokens.extend(nltk.word_tokenize(post['description'].lower()))
+    id = str(post['_id'])
+    documents.append(id)
+
+'''
+stopset= set(stopwords.words('english'))
+table = tfidf.tfidf()
+counter=0
+for post in collection.find():
+    counter = counter + 1
+    if counter % 10000 == 0:
+        print counter
+    tmp_tokens= nltk.word_tokenize(post['description'].lower())
+    id = str(post['_id'])
+    documents.append(id)
+    doc_tokens = []
+    for t in tmp_tokens:
+        if t not in stopset:
+            doc_tokens.append(t)
+    #print doc_tokens
+    table.addDocument(id, doc_tokens)
+    global_doc[id]= doc_tokens
+
+pickle.dump( table, open( "table.p", "wb" ) )
+print 'finished tokenizing'
+'''
+
+corpus = pickle.load( open( "table.p", "rb" ) )
+counter=0
+
+for id in documents:
+    counter = counter + 1
+    print counter
+    results[id]= corpus.similarities(global_doc[id])
+
+#print results
+pickle.dump( results, open( "similarities.p", "wb" ) )
+
+
+
+'''
 text = nltk.Text(tokens)
 fd = nltk.FreqDist(text)
 print 'triangle', fd['triangle']
@@ -40,3 +86,4 @@ fd.plot(100,cumulative=False)
     #for grams in sixgrams:
     #    print
     #print post
+'''
