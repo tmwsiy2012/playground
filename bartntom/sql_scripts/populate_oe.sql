17d08118653626bd8d7ff1b5ca4ea958d5369ae3
Eddie Dunn <eddie@eddiedunn.com>
2015-05-08 15:56:59 -0400

    asdf

--- bartntom/sql_scripts/populate_oe.sql
+++ bartntom/sql_scripts/populate_oe.sql
@@ -52,5 +52,5 @@ LOAD DATA INFILE 'C:/Users/tmwsiy/Downloads/oe_raw/oe.series'
 	INTO TABLE series FIELDS TERMINATED BY '\t'
 	LINES TERMINATED BY '\r\n'  IGNORE 1 LINES
 	(series_id, seasonal, areatype_code, industry_code, occupation_code, datatype_code,
-   state_code, area_code, sector_code, series_title, footnote_code,
+   state_code, area_code, sector_code, series_title, footnote_codes,
    begin_year, begin_period, end_year, end_period);
